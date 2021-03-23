92efca3db3b48e7f2f007227df9d7a537b2d2873
Will Esquivel <williamc.esquivel@gmail.com>
2021-03-23 15:13:47 -0500

    added q's 1-5 in html

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
 var start = document.getElementById('start');
 var header = document.getElementById('header');
 var para1 = document.getElementById('para1');
+let currentQuesIndex = 0;
+
 
 var questions = [{
-    name: 'What does HTML stand for?',
+    text: 'What does HTML stand for?',
     options: ['hypertext markup language', 'hyperloop makeup language', 'hypertank markdown language', 'none of the above'],
     correct: 'hypertext markup language'
 },
 {
-    name: 'Commonly used data types DO NOT include:',
+    text: 'Commonly used data types DO NOT include:',
     options: ['strings', 'booleans', 'alerts', 'numbers'], 
     correct: 'booleans'
 },
 {
-    name: 'The condition in an if / else statement is enclosed within?',
+    text: 'The condition in an if / else statement is enclosed within _____ ? ',
     options: ['quotes', 'curly-brackets', 'parentheses', 'square-brackets'],
     correct: 'parentheses'
 },
 {
-    name: 'Arrays in JavaScript can be used to store:',
+    text: 'Arrays in JavaScript can be used to store:',
     options: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
     correct: 'all of the above'
 },
 {
-    name:'String values must be enclosed within _____ when being assigned to variables.',
-    options: ['commas', 'curly-brakcets', 'quotes', 'parentheses'],
+    text:'String values must be enclosed within _____ when being assigned to variables.',
+    options: ['commas', 'curly-brackets', 'quotes', 'parentheses'],
     correct: 'curly-brackets'
 }];
+
+let currentQues = questions[currentQuesIndex];
 
 start.onclick = StartQuiz;
 
@@ -36,6 +40,11 @@ function StartQuiz() {
     header.style.display = 'none';
     para1.style.display = 'none';
 
-    console.log('X');
+    for (let i = 0; i < questions[0].length; i++) {
+        const question = questions[i];
+        
+    }
+
+    console.log(StartQuiz);
 };
 
